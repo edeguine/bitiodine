204c6c68c12e1ad1df75d3735e5efddcfd59825b
Michele Spagnuolo <michele@spagnuolo.me>
2013-10-18 11:57:07 +0200

    Added chmod

--- blockparser/cb/sql.cpp
+++ blockparser/cb/sql.cpp
@@ -332,6 +332,7 @@ struct SQLDump:public Callback
         fclose(inputFile);
         fclose(blockFile);
         fclose(txFile);
+        system("chmod +x ./blockChain.sh")
         int ret = system("./blockChain.sh");
         if (!ret) {
             info("Error in executing script!");
