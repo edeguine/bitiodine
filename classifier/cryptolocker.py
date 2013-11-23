3b07837018f9d0dee4641b028d8e249851882c6d
Michele Spagnuolo <michele@spagnuolo.me>
2013-11-23 18:50:37 +0100

    asd

--- classifier/cryptolocker.py
+++ classifier/cryptolocker.py
@@ -24,6 +24,8 @@ print("Clusters loaded.")
 for addr in known:
 	clusters.add(users[addr])
 
+del(users)
+
 print("%d clusters found." % len(clusters))
 
 for cluster in clusters:
@@ -31,18 +33,28 @@ for cluster in clusters:
 
 clusters_query = '(' + ', '.join([str(k) for k in clusters]) + ')'
 
-sum_query = "SELECT SUM(txout_value)/1e8 FROM tx_full WHERE address IN " + clusters_query + " AND ((txout_value BETWEEN 1.98e8 AND 2.01e8) OR (txout_value BETWEEN 0.48e8 AND 0.52e8) OR (txout_value BETWEEN 9.98e8 AND 10.02e8));"
+sum_query = "SELECT SUM(txout_value)/1e8 FROM tx_full WHERE address IN " + clusters_query + " AND ((txout_value BETWEEN 1.98e8 AND 2.01e8) OR (txout_value BETWEEN 0.48e8 AND 0.52e8) OR (txout_value BETWEEN 9.98e8 AND 10.02e8))"
 
-detail_query = "SELECT address, COUNT(*) AS ransoms FROM tx_full WHERE address IN " + clusters_query + " AND ((txout_value BETWEEN 1.98e8 AND 2.01e8) OR (txout_value BETWEEN 0.48e8 AND 0.52e8) OR (txout_value BETWEEN 9.98e8 AND 10.02e8)) GROUP BY address ORDER by ransoms DESC;"
+detail_query = "SELECT address, COUNT(*) AS ransoms FROM tx_full WHERE address IN " + clusters_query + " AND ((txout_value BETWEEN 1.98e8 AND 2.01e8) OR (txout_value BETWEEN 0.48e8 AND 0.52e8) OR (txout_value BETWEEN 9.98e8 AND 10.02e8)) GROUP BY address ORDER by ransoms DESC"
+
+tx_query = "SELECT datetime(time, 'unixepoch'), tx_hash, address FROM tx_full WHERE address IN " + clusters_query + " AND ((txout_value BETWEEN 1.98e8 AND 2.01e8) OR (txout_value BETWEEN 0.48e8 AND 0.52e8) OR (txout_value BETWEEN 9.98e8 AND 10.02e8)) ORDER BY time ASC"
 
 sum_res = float(db_blockchain.query(sum_query, fetch_one=True))
 
 print("Sum: %f" % sum_res)
-print()
-print()
 
 detail_res = db_blockchain.query(detail_query)
+tx_res = db_blockchain.query(tx_query)
 
-for row in detail_res:
-	for address, ransoms in row:
-		print("%s, %d" % (address, int(ransoms)))
+with open("cryptolocker_ransoms.txt", "w") as rf:
+	for row in detail_res:
+		for address, ransoms in row:
+			print("%s, %d" % (address, int(ransoms)))
+			rf.write("%s, %d" % (address, int(ransoms)))
+
+with open("cryptolocker_tx.txt", "w") as tf:
+	for row in detail_res:
+		for datetime, tx_hash, address in row:
+			print("%s, %s, %s" % (datetime, tx_hash, address))
+			rf.write("%s, %s, %s" % (datetime, tx_hash, address))
+
